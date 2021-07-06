dde17b33b90b5d767235f8a618db7f23627d2ea5
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-06 13:48:35 -0300

    [2 - Basic Queries] Basic useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useQuery } from "react-query";
+import axios from "axios";
+
+export interface Pokemon {
+  name: string;
+  url: string;
+}
+
 function App() {
+  const queryInfo = useQuery<Pokemon[]>("pokemon", () =>
+    axios("https://pokeapi.co/api/v2/pokemon").then((res) => res.data.results),
+  );
 
   console.log(useQuery);
 
@@ -11,6 +21,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        {queryInfo.data?.length &&
+          queryInfo.data.map((pokemon) => (
+            <div key={pokemon.name}>{pokemon.name}</div>
+          ))}
       </header>
     </div>
   );

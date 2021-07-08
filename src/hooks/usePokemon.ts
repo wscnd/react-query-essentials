51973bc88392c981673e7acc29675db159cdfd89
wscnd <72019998+wscnd@users.noreply.github.com>
2021-07-07 21:29:24 -0300

    extract usePokemon queryFn

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -6,11 +6,13 @@ export type PokeApiResponse = {
   results: Pokemon[];
 };
 
+export const fetchPokemonList = async () => {
+  await new Promise((res) => setTimeout(res, 2000));
+  return axios
+    .get<PokeApiResponse>("https://pokeapi.co/api/v2/pokemon")
+    .then((response) => response.data.results);
+};
+
 export const usePokemon = () => {
-  return useQuery<Pokemon[]>("pokemon", async () => {
-    await new Promise((res) => setTimeout(res, 2000));
-    return axios
-      .get<PokeApiResponse>("https://pokeapi.co/api/v2/pokemon")
-      .then((response) => response.data.results);
-  });
+  return useQuery<Pokemon[]>("pokemon", fetchPokemonList);
 };
